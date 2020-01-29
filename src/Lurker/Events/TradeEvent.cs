f8a86e5bb4abd62b6c5911d8193ab0c11ca4e8cf
tamaroth <tamaroth@tamaroth.eu>
2020-01-29 21:51:55 +0100

    Correctly process log line that does not contain location.
    Some whispers do not contain the location of the item. For instance when found on bulk trade. If location of an item could not be determined, use generic location instead.

--- src/Lurker/Events/TradeEvent.cs
+++ src/Lurker/Events/TradeEvent.cs
@@ -52,6 +52,10 @@ namespace Lurker.Events
             {
                 this.Location = this.ParseLocation(textAfterItemName.Substring(locationMarkerIndex));
             }
+            else
+            {
+                this.Location = new Location();
+            }
 
             // Price
             if (priceMarkerIndex != -1)
