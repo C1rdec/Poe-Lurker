f3f434ce6acef07f436ad2be0d0e2aa924ef94f6
C1rdec <cedric_lampron@hotmail.com>
2020-04-07 01:07:23 -0400

    Add Firebase Cloud Messaging (FCM)

--- src/poe_lurker_mobile/lib/main.dart
+++ src/poe_lurker_mobile/lib/main.dart
@@ -2,11 +2,14 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  final FirebaseMessaging _fcm = FirebaseMessaging();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -18,9 +21,52 @@ class MyApp extends StatelessWidget {
           title: Text('Poe Lurker'),
         ),
         body: Center(
-          child: Text('Hello World'),
+          child: MessageHandler(),
         ),
       ),
     );
+  }
+
+  Future<String> getTokenAsync() async {
+    return this._fcm.getToken();
+  }
+}
+
+class MessageHandler extends StatefulWidget {
+  @override
+  MessageHandlerState createState() => MessageHandlerState();
+}
+
+class MessageHandlerState extends State<MessageHandler> {
+  final FirebaseMessaging _fcm = FirebaseMessaging();
+  String token = "Loading";
+
+ @override
+ void initState() {
+    super.initState();
+    this._fcm.configure(
+      onMessage: (Map<String, dynamic> message) async {
+        print("onMessage: $message");
+      },
+      onLaunch: (Map<String, dynamic> message) async {
+        print("onLaunch: $message");
+      },
+      onResume: (Map<String, dynamic> message) async {
+        print("onResume: $message");
+      } 
+    );
+    getTokenAsync();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(this.token);
+  }
+
+  void getTokenAsync() async {
+    var test = await this._fcm.getToken();
+    setState(() {
+      token = test;
+    });
   }
 }
