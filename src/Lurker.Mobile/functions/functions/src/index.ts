03adbbe0e9cb00342eb402c3e6e7ad40285a81c2
C1rdec <cedric_lampron@hotmail.com>
2020-04-20 21:58:13 -0400

    Add firebase admin to the function

--- src/Lurker.Mobile/functions/functions/src/index.ts
+++ src/Lurker.Mobile/functions/functions/src/index.ts
@@ -1,8 +1,14 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+
+admin.initializeApp();
+const fcm = admin.messaging();
+const token = "[Token]";
 
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 
-export const helloWorld = functions.https.onRequest((request, response) => {
+export const sendTradeMessage = functions.https.onRequest((request, response) => {
  response.send("Hello from Firebase!");
+ return fcm.send({token: token, data: {test:"allo"}});
 });
