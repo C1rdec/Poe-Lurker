98b0484a4990aed63df9c6e1529a4be0e63be813
C1rdec <cedric_lampron@hotmail.com>
2020-10-30 22:11:48 -0400

    Added MapViewModel.

---
+++ src/Lurker.UI/ViewModels/MapViewModel.cs
@@ -0,0 +1,114 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="MapViewModel.cs" company="Wohs Inc.">
+//     Copyright © Wohs Inc.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace Lurker.UI.ViewModels
+{
+    using System.Linq;
+    using Lurker.Patreon.Models;
+    using Sentry.Protocol;
+
+    /// <summary>
+    /// Represents a map item.
+    /// </summary>
+    /// <seealso cref="Caliburn.Micro.PropertyChangedBase" />
+    public class MapViewModel : Caliburn.Micro.PropertyChangedBase
+    {
+        #region Fields
+
+        private const string ReflectPhysicalId = "stat_3464419871";
+        private const string ReflectElementalId = "stat_2764017512";
+        private const string CannotRegenerateId = "stat_1910157106";
+        private const string CannotLeechId = "stat_526251910";
+        private const string TemporalChainsId = "stat_2326202293";
+        private Map _map;
+        private bool _reflectPhysical;
+        private bool _reflectElemental;
+        private bool _cannotRegenerate;
+        private bool _cannotLeech;
+        private bool _temporalChains;
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MapViewModel"/> class.
+        /// </summary>
+        /// <param name="map">The map.</param>
+        public MapViewModel(Map map)
+        {
+            this._map = map;
+            foreach (var affix in this._map.DangerousAffixes.Where(d => d != null))
+            {
+                switch (affix.Id)
+                {
+                    case ReflectPhysicalId:
+                        this._reflectPhysical = true;
+                        break;
+                    case ReflectElementalId:
+                        this._reflectElemental = true;
+                        break;
+                    case CannotRegenerateId:
+                        this._cannotRegenerate = true;
+                        break;
+                    case CannotLeechId:
+                        this._cannotLeech = true;
+                        break;
+                    case TemporalChainsId:
+                        this._temporalChains = true;
+                        break;
+                }
+            }
+        }
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets a value indicating whether [reflect physical].
+        /// </summary>
+        public bool ReflectPhysical => this._reflectPhysical;
+
+        /// <summary>
+        /// Gets a value indicating whether [reflect elemental].
+        /// </summary>
+        public bool ReflectElemental => this._reflectElemental;
+
+        /// <summary>
+        /// Gets a value indicating whether [cannot regenerate].
+        /// </summary>
+        public bool CannotRegenerate => this._cannotRegenerate;
+
+        /// <summary>
+        /// Gets a value indicating whether [cannot leech].
+        /// </summary>
+        public bool CannotLeech => this._cannotLeech;
+
+        /// <summary>
+        /// Gets a value indicating whether [temporal chains].
+        /// </summary
+        public bool TemporalChains => this._temporalChains;
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Notifies the change.
+        /// </summary>
+        public void NotifyChange()
+        {
+            this.NotifyOfPropertyChange(() => this.ReflectPhysical);
+            this.NotifyOfPropertyChange(() => this.ReflectElemental);
+            this.NotifyOfPropertyChange(() => this.CannotRegenerate);
+            this.NotifyOfPropertyChange(() => this.CannotLeech);
+            this.NotifyOfPropertyChange(() => this.TemporalChains);
+        }
+
+        #endregion
+    }
+}
