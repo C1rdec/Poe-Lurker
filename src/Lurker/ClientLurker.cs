83400448235231bf9d0a2ecde86567d51a25d28d
C1rdec <cedric_lampron@hotmail.com>
2021-04-13 22:19:22 -0400

    Fixed Sentry BreadCrum

--- src/Lurker/ClientLurker.cs
+++ src/Lurker/ClientLurker.cs
@@ -413,7 +413,7 @@ namespace Lurker
                 Logger.Error(exception, exception.Message);
 
 #if !DEBUG
-                SentrySdk.AddBreadcrumb(message: lineError, level: Sentry.Protocol.BreadcrumbLevel.Error);
+                SentrySdk.AddBreadcrumb(message: lineError, level: BreadcrumbLevel.Error);
                 SentrySdk.CaptureException(ex);
 #endif
             }
