8d2982236022e4219a31c6c2b873416c8fd0cb78
C1rdec <cedric_lampron@hotmail.com>
2021-05-19 19:31:20 -0400

    Increment assembly version.

--- src/Lurker.UI/Properties/AssemblyInfo.cs
+++ src/Lurker.UI/Properties/AssemblyInfo.cs
@@ -58,5 +58,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.9.11.0")]
-[assembly: AssemblyFileVersion("1.9.11.0")]
+[assembly: AssemblyVersion("1.9.12.0")]
+[assembly: AssemblyFileVersion("1.9.12.0")]
