8b92e1aa7826142e65f28e5e542be6aec0714eb4
C1rdec <cedric_lampron@hotmail.com>
2021-07-22 12:10:46 -0400

    Add partial PoB gem parser

--- src/Lurker.DataParser/GemParser.cs
+++ src/Lurker.DataParser/GemParser.cs
@@ -9,8 +9,8 @@ namespace Lurker.DataParser
     using System;
     using System.Collections.Generic;
     using System.Linq;
-    using System.Net.Http;
-    using Newtonsoft.Json.Linq;
+    using Lurker.DataParser.Gems;
+    using Lurker.Models;
 
     public class GemParser
     {
@@ -18,63 +18,35 @@ namespace Lurker.DataParser
 
         public static string Parse()
         {
-            var fileUrl = "https://raw.githubusercontent.com/brather1ng/RePoE/master/RePoE/data/gems.json";
-            var client = new HttpClient();
-            var request = new HttpRequestMessage(HttpMethod.Get, fileUrl);
-            var response = client.SendAsync(request).Result;
+            var gems = RePoeParser.Parse();
+            //var gems = PathOfBuildingParser.Parse();
 
-            var gemInfo = response.Content.ReadAsStringAsync().Result;
-
-            var jobject = JObject.Parse(gemInfo);
-
-            var gems = new List<Models.Gem>();
-            foreach (var element in jobject.Children())
+            var count = 1;
+            var totalCount = gems.Count();
+            foreach (var gem in gems)
             {
-                Console.Write($"\rParsing Gems information... ({gems.Count()})");
-
-                var children = element.Children();
-
-                var requiredLevel = children["per_level"]["1"]["required_level"].FirstOrDefault();
-                if (requiredLevel == null)
-                {
-                    continue;
-                }
-
-                var isSupport = (bool)children["is_support"].FirstOrDefault();
-                var id = ((Newtonsoft.Json.Linq.JProperty)element).Name;
-
-                var baseItem = children["base_item"].FirstOrDefault();
-                if (baseItem is JValue)
-                {
-                    continue;
-                }
-
-                var displayName = (string)children["base_item"]["display_name"].FirstOrDefault();
-                var gem = new Models.Gem()
-                {
-                    Id = id,
-                    Name = displayName,
-                    Level = (int)requiredLevel,
-                    Support = isSupport,
-                };
-
+                Console.Write($"\rParsing Gems wiki information... ({count}/{totalCount})");
                 gem.ParseWiki();
-                gems.Add(gem);
-            }
+                count++;
 
-            // Update Location for Vaal Gems
-            foreach (var gem in gems.Where(g => g.Name.StartsWith("Vaal ")))
-            {
-                var normalGem = gems.FirstOrDefault(g => g.Name == gem.Name.Substring(5, gem.Name.Length - 5));
-                if (normalGem == null)
+                if (gem.Name.StartsWith("Vaal "))
                 {
-                    continue;
-                }
+                    var normalGem = gems.FirstOrDefault(g => g.Name == gem.Name.Substring(5, gem.Name.Length - 5));
+                    if (normalGem == null)
+                    {
+                        continue;
+                    }
 
-                gem.Location = normalGem.Location;
+                    gem.Location = normalGem.Location;
+                }
             }
 
             return Newtonsoft.Json.JsonConvert.SerializeObject(gems, Newtonsoft.Json.Formatting.Indented);
+        }
+
+        private static IEnumerable<Gem> ParsePathOfBuilding()
+        {
+            return Enumerable.Empty<Gem>();
         }
 
         #endregion
---
+++ src/Lurker.DataParser/Gems/RePoeParser.cs
@@ -0,0 +1,63 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="RePoeParser.cs" company="Wohs Inc.">
+//     Copyright © Wohs Inc.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace Lurker.DataParser.Gems
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Net.Http;
+    using Lurker.Models;
+    using Newtonsoft.Json.Linq;
+
+    public static class RePoeParser
+    {
+        public static IEnumerable<Gem> Parse()
+        {
+            var fileUrl = "https://raw.githubusercontent.com/brather1ng/RePoE/master/RePoE/data/gems.json";
+            var client = new HttpClient();
+            var request = new HttpRequestMessage(HttpMethod.Get, fileUrl);
+            var response = client.SendAsync(request).Result;
+
+            var gemInfo = response.Content.ReadAsStringAsync().Result;
+
+            var jobject = JObject.Parse(gemInfo);
+
+            var gems = new List<Gem>();
+            foreach (var element in jobject.Children())
+            {
+                var children = element.Children();
+
+                var requiredLevel = children["per_level"]["1"]["required_level"].FirstOrDefault();
+                if (requiredLevel == null)
+                {
+                    continue;
+                }
+
+                var isSupport = (bool)children["is_support"].FirstOrDefault();
+                var id = ((JProperty)element).Name;
+
+                var baseItem = children["base_item"].FirstOrDefault();
+                if (baseItem is JValue)
+                {
+                    continue;
+                }
+
+                var displayName = (string)children["base_item"]["display_name"].FirstOrDefault();
+                var gem = new Gem()
+                {
+                    Id = id,
+                    Name = displayName,
+                    Level = (int)requiredLevel,
+                    Support = isSupport,
+                };
+
+                gems.Add(gem);
+            }
+
+            return gems;
+        }
+    }
+}
