4286cdd745a31543cf35a05b43fb9063491edab1
Cédric Lampron <cedric_lampron@hotmail.com>
2025-10-23 21:17:28 -0400

    Add curse affix on waystone

--- src/PoeLurker.UI/ViewModels/WaystoneViewModel.cs
+++ src/PoeLurker.UI/ViewModels/WaystoneViewModel.cs
@@ -9,15 +9,23 @@ public class WaystoneViewModel : PropertyChangedBase
     private Map _map;
     private List<Affix> _damageAffixes;
     private List<Affix> _dangerousAffixes;
+    private List<Affix> _curseAffixes;
 
     public WaystoneViewModel(Map map)
     {
         _map = map;
         _damageAffixes = map.DamageAffixes.ToList();
         _dangerousAffixes = map.DangerousAffixes.ToList();
+        _curseAffixes = map.CurseAffixes.ToList();
     }
 
-    public bool ReflectPhysical => _dangerousAffixes.Any(d => d.Text.Contains("Physical Damage"));
+    public bool TemporalChain => _curseAffixes.Any(d => d.Text.Contains("Temporal Chains"));
+
+    public bool ElementalWeakness => _curseAffixes.Any(d => d.Text.Contains("Elemental Weakness"));
+
+    public bool Vulnerability => _curseAffixes.Any(d => d.Text.Contains("Vulnerability"));
+
+    public bool Enfeeble => _curseAffixes.Any(d => d.Text.Contains("Enfeeble"));
 
     public bool ReflectElemental => _dangerousAffixes.Any(d => d.Text.Contains("Elemental Damage"));
 
@@ -38,6 +46,8 @@ public class WaystoneViewModel : PropertyChangedBase
     public bool CritOrDamage => Damage || Critical;
 
     public bool NoElemental => !(Safe || FireDamage || ColdDamage || LightningDamage || ChaosDamage);
+
+    public bool HasCurse => _curseAffixes.Any();
 
     public int PackSize => _map.PackSize;
 
